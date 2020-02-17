f5f47f3a4a3484b5966efa21afc844e753550521
dangkelly4 <dangkelly4@gmail.com>
2020-02-16 23:55:48 -0800

    completed button
    created buttons that will change color when clicked

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnTest = document.querySelector('#test');
+let btnRSVP = document.querySelector('#RSVP');
+
+btnTest.addEventListener('click', () => btnTest.style.backgroundColor = '#337ab7')
+btnRSVP.addEventListener('click', () => btnRSVP.style.backgroundColor = '#4cae4c')
