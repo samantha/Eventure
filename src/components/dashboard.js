bd41a891c79ef149b9f5f943c9c6ade157bf420a
Kareena <kareenajc@users.noreply.github.com>
2020-02-26 09:44:27 -0800

    responsive navbar with React Bootstrap

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,68 +4,76 @@ import TopNav from "./topnav.js";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar, faClock, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import '../dashboard.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class Dashboard extends Component {
     render() {
       return (
-        <div>            
-            <TopNav/>
-            <main>
-              <h1>UPCOMING EVENTS</h1>
-              <section class="event-tiles">
-                  <section class="tile">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
-                    <section class="event-details">
-                      <h2>Event Name</h2>
-                      <p>Organization</p>
-                      <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
-                      <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
-                      <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+        <Router>
+          <div>            
+              <TopNav/>
+              <main>
+                <h1>UPCOMING EVENTS</h1>
+                <section class="event-tiles">
+                    <section class="tile">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
+                      <section class="event-details">
+                        <h2>Event Name</h2>
+                        <p>Organization</p>
+                        <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
+                        <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                      </section>
                     </section>
-                  </section>
-                  <section class="tile">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
-                    <section class="event-details">
-                      <h2>Event Name</h2>
-                      <p>Organization</p>
-                      <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
-                      <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
-                      <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                    <section class="tile">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
+                      <section class="event-details">
+                        <h2>Event Name</h2>
+                        <p>Organization</p>
+                        <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
+                        <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                      </section>
                     </section>
-                  </section>
-                  <section class="tile">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
-                    <section class="event-details">
-                      <h2>Event Name</h2>
-                      <p>Organization</p>
-                      <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
-                      <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
-                      <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                    <section class="tile">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
+                      <section class="event-details">
+                        <h2>Event Name</h2>
+                        <p>Organization</p>
+                        <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
+                        <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                      </section>
                     </section>
-                  </section>
-                  <section class="tile">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
-                    <section class="event-details">
-                      <h2>Event Name</h2>
-                      <p>Organization</p>
-                      <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
-                      <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
-                      <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                    <section class="tile">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
+                      <section class="event-details">
+                        <h2>Event Name</h2>
+                        <p>Organization</p>
+                        <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
+                        <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                      </section>
                     </section>
-                  </section>
-                  <section class="tile">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
-                    <section class="event-details">
-                      <h2>Event Name</h2>
-                      <p>Organization</p>
-                      <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
-                      <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
-                      <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                    <section class="tile">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Golden_Gate_Bridge_at_sunset_1.jpg/1600px-Golden_Gate_Bridge_at_sunset_1.jpg"></img>
+                      <section class="event-details">
+                        <h2>Event Name</h2>
+                        <p>Organization</p>
+                        <p><FontAwesomeIcon icon={faCalendar}/><time datetime=""> Date</time></p>
+                        <p><FontAwesomeIcon icon={faClock}/><time datetime=""> Time</time></p>
+                        <p><FontAwesomeIcon icon={faMapMarkerAlt}/> Location</p>
+                      </section>
                     </section>
-                  </section>
-              </section>
-            </main>            
-        </div>
+                </section>
+              </main>            
+          </div>
+        </Router>
       );
     }
 }
--- src/components/topnav.js
+++ src/components/topnav.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import '../nav.css';
 import Logo from '../logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faBell, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faBell, faUserCircle, faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
+import {Navbar, NavDropdown, Form, FormControl, Button, Nav} from 'react-bootstrap';
 
 export default class TopNav extends Component {
     render() {
         return (
-                <div class="nav nav-container">
+            <div>
+                <div class="nav nav-container" id="nav-desktop">
                     <div class="nav">
                         <div><a href="./dashboard.js"><img src="" alt="Eventure Logo"/></a></div>
                         <div><input class="form-control" type="text" placeholder="Search"></input></div>
@@ -18,6 +20,25 @@ export default class TopNav extends Component {
                         <div><FontAwesomeIcon icon={faUserCircle}/> Profile</div>
                     </div>
                 </div>
+
+                <div class="nav-mobile">
+                    <Navbar bg="light" expand="lg">
+                        <Navbar.Brand href="#home"><img src="#" alt="Eventure Logo"/></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#home">Messages</Nav.Link>
+                                <Nav.Link href="#link">Notifications</Nav.Link>
+                                <Nav.Link href="#link">Profile</Nav.Link>
+                            </Nav>
+                            <Form inline>
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse> 
+                    </Navbar>
+                </div>
+            </div>               
         );
     }
 }
