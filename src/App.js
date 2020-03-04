d254ff53232d0857f8e08b7deb8286e8284af6bb
samantha <shangsan@ucla.edu>
2020-03-04 14:21:57 -0800

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /// COMPONENTS ///
+import Navigation from "./components/navbar";
 import Home from "./components/home";
 import CreateEvent from "./components/create-event";
 
@@ -14,6 +15,7 @@ import CreateEvent from "./components/create-event";
 function App() {
   return (
     <Router>
+      <Navigation />
       <Route path="/" exact component = {Home} />
       <Route path="/create" component = {CreateEvent} />
 
