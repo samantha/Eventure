0fd1acff42177298b2852eda858df496225b8e9b
samantha <shangsan@ucla.edu>
2020-05-10 21:40:21 -0700

    Formatted Manage Events Table

--- frontend/src/components/tables/EventTable.js
+++ frontend/src/components/tables/EventTable.js
@@ -1,41 +1,43 @@
 import React, { Component } from "react";
 import { Table, Button } from "reactstrap";
-import ModalForm from "../modals/ModalForm";
+// import ModalForm from '../modals/ModalForm'
+import "../../styles/table.css";
 
 class EventTable extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: this.props.currentUser,
-      orgItems: null,
+      user: this.props.user,
+      eventItems: null,
     };
-    this.getUserManagedOrgs = this.getUserManagedOrgs.bind(this);
+    this.getUserManagedEvents = this.getUserManagedEvents.bind(this);
+    this.editEvent = this.editEvent.bind(this);
   }
 
-  getUserManagedOrgs() {
-    console.log("get user orgs");
-    fetch("http://localhost:3000/userorgs", {
+  getUserManagedEvents() {
+    console.log(this.props.user.username);
+    fetch("http://localhost:3000/usermanagedevents", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: this.props.currentUser.username,
+        username: this.props.user.username,
       }),
     })
       .then((response) => response.json())
       .then((item) => {
         if (Array.isArray(item)) {
-          const orgItems = item.map((org) => (
-            <div className="my-orgs">
-              <a href={"/o/" + org.handle}>{org.name}</a>
-            </div>
-          ));
+          // const eventItems = item.map((org) => (
+          //   <div className="my-orgs">
+          //     <a href={"/o/" + org.handle}>{org.name}</a>
+          //   </div>
+          // ));
           this.setState({
-            orgItems: orgItems,
+            eventItems: item,
           });
-          console.log(this.state.orgItems);
+          console.log(this.state.eventItems);
         } else {
           console.log("failure");
         }
@@ -43,63 +45,79 @@ class EventTable extends Component {
       .catch((err) => console.log(err));
   }
 
-  deleteItem = (id) => {
+  deleteEvent = (item) => {
     let confirmDelete = window.confirm("Delete item forever?");
     if (confirmDelete) {
-      fetch("http://localhost:3000/crud", {
+      fetch("http://localhost:3000/events", {
         method: "delete",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          id,
+          handle: item.handle,
         }),
       })
         .then((response) => response.json())
         .then((item) => {
-          this.props.deleteItemFromState(id);
+          if (Array.isArray(item)) {
+            this.getUserManagedEvents();
+          } else {
+            console.log("failure");
+          }
         })
         .catch((err) => console.log(err));
     }
   };
 
+  editEvent = (item) => {
+    this.props.history.push("/settings/event/" + item.handle);
+  };
+
+  componentDidMount() {
+    this.getUserManagedEvents();
+  }
+
   render() {
-    // const items = this.state.orgItems.map(item => {
-    //   return (
-    //     <tr key={item.id}>
-    //       <th scope="row">{item.id}</th>
-    //       <td>{item.name}</td>
-    //       <td>{item.last}</td>
-    //       <td>{item.email}</td>
-    //       <td>{item.phone}</td>
-    //       <td>{item.location}</td>
-    //       <td>{item.hobby}</td>
-    //       <td>
-    //         <div style={{width:"110px"}}>
-    //           <ModalForm buttonLabel="Edit" item={item} updateState={this.props.updateState}/>
-    //           {' '}
-    //           <Button color="danger" onClick={() => this.deleteItem(item.id)}>Del</Button>
-    //         </div>
-    //       </td>
-    //     </tr>
-    //     )
-    //   })
+    let items;
+    if (this.state.eventItems) {
+      console.log(this.state.eventItems);
+      items = this.state.eventItems.map((item) => {
+        return (
+          <tr key={item.handle}>
+            <th scope="row">{item.handle}</th>
+            <td>{item.name}</td>
+            <td>{item.description}</td>
+            <td>{item.city}</td>
+            <td>{item.state}</td>
+
+            <td>
+              <div style={{ width: "110px" }}>
+                <Button color="warning" onClick={() => this.editEvent(item)}>
+                  Edit
+                </Button>{" "}
+                <Button color="danger" onClick={() => this.deleteEvent(item)}>
+                  Del
+                </Button>
+              </div>
+            </td>
+          </tr>
+        );
+      });
+    }
 
     return (
-      <Table responsive hover>
+      <Table responsive hover dark striped size="sm">
+        <caption>Manage Events</caption>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>First</th>
-            <th>Last</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Location</th>
-            <th>Hobby</th>
-            <th>Actions</th>
+            <th>Handle</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>City</th>
+            <th>State</th>
           </tr>
         </thead>
-        <tbody>{/*{items}*/}</tbody>
+        <tbody>{items}</tbody>
       </Table>
     );
   }
